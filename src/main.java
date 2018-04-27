7020d163ecebd4303e04a08aaa7e370b1fa4cde8
BGarner19 <bailey.garner@asu.edu>
2018-04-26 21:36:42 -0700

    Gitignore updates and redundant main variables

--- src/main.java
+++ src/main.java
@@ -4,8 +4,7 @@ import java.sql.Statement;
 
 public class main {
     public static void main(String args[]) {
-        Connection c = null;
-        String sql;
+        Connection c;
         Database db = new Database();
 
         try {
