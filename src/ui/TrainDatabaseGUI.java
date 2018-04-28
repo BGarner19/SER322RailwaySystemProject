0e5b62026bf3c019aab534ff892690a12a495e9f
Rachel Knoche <rknoche@asu.edu>
2018-04-27 22:44:53 -0700

    add passenger queries

--- src/ui/TrainDatabaseGUI.java
+++ src/ui/TrainDatabaseGUI.java
@@ -993,13 +993,23 @@ public class TrainDatabaseGUI {
 
 				if(ticketType.equals("") && tripID.equals("")){
 					sql = "SELECT FName, MI, LName FROM Railway.PASSENGERS";
-				} else if(!(ticketType.equals("")) && tripID.equals("")){
+				} else if (ticketType.equals("")){
+					sql = "SELECT FName, MI, LName " +
+							"FROM railway.PASSENGERS " +
+							"WHERE ID IN (SELECT ID FROM railway.TICKETS WHERE TripID IN " +
+							"(SELECT ID FROM railway.TRIPS WHERE ID = '" + tripID + "'))";
+				} else if(tripID.equals("")){
 					sql = "SELECT FName, MI, LName " +
 							"FROM railway.PASSENGERS " +
 							"WHERE ID IN (SELECT ID FROM railway.TICKETS WHERE TypeID IN " +
 							"(SELECT ID FROM railway.TICKET_TYPES WHERE Type = '" + ticketType + "'))";
-
-					System.out.print("emptytrip");
+				} else {
+					sql = "SELECT FName, MI, LName " +
+							"FROM railway.PASSENGERS " +
+							"WHERE ID IN (SELECT ID FROM railway.TICKETS WHERE TripID IN " +
+							"(SELECT ID FROM railway.TRIPS WHERE ID = '" + tripID + "') AND" +
+							"(ID IN (SELECT ID FROM railway.TICKETS WHERE TripID IN " +
+							"(SELECT ID FROM railway.TRIPS WHERE ID = '" + tripID + "'))))";
 				}
 
 
